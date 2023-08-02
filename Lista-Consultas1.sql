9a11a48cbc7b503ee9cb0d2caf85eb54344b4240
Ellen172 <ellencas172@gmail.com>
2023-08-02 19:27:45 -0300

    lista 1 resolvida

--- Lista-Consultas1.sql
+++ Lista-Consultas1.sql
@@ -1,47 +1,47 @@
 ﻿--01-Nomes das Agências com depósitos (nome pode aparecer repetido)
 
-SELECT nome_agencia FROM ;
+SELECT nome_agencia FROM agencia;
 
 --02-Nomes das Agências com depósitos (nome sem repetição)
 
-SELECT DISTINCT  FROM deposito;
+SELECT DISTINCT nome_agencia FROM deposito;
 
 --03-Nomes de Clientes com depósitos e empréstimos ao mesmo tempo;
 
-SELECT DISTINCT nome_cliente FROM 
+SELECT DISTINCT nome_cliente FROM deposito
 intersect 
-SELECT DISTINCT  FROM emprestimo
+SELECT DISTINCT nome_cliente FROM emprestimo
 
 --04-Nomes de Clientes com depósitos e empréstimos ao mesmo tempo na agência PUC;
 
-SELECT DISTINCT nome_cliente FROM  WHERE nome_agencia=  
+SELECT DISTINCT nome_cliente FROM deposito WHERE nome_agencia='PUC'
 intersect 
-SELECT DISTINCT nome_cliente FROM emprestimo WHERE ='PUC'
+SELECT DISTINCT nome_cliente FROM emprestimo WHERE nome_agencia='PUC'
 
 --Alternativa2 para incluir a condição uma única vez
 SELECT DISTINCT nome_cliente FROM 
-	(	SELECT DISTINCT nome_cliente, nome_agencia FROM 
+	(	SELECT DISTINCT nome_cliente, nome_agencia FROM deposito
 		intersect 
-		select DISTINCT  nome_agencia FROM emprestimo
+		select DISTINCT nome_cliente, nome_agencia FROM emprestimo
 	) AS relatorio
 WHERE relatorio.nome_agencia = 'PUC'
 
 --Alternativa3 para usar outro operador (IN)
-SELECT DISTINCT nome_cliente FROM deposito WHERE nome_agencia= 
-AND nome_cliente  (SELECT DISTINCT nome_cliente FROM emprestimo WHERE nome_agencia='PUC');
+SELECT DISTINCT nome_cliente FROM deposito WHERE nome_agencia='PUC'
+AND nome_cliente in (SELECT DISTINCT nome_cliente FROM emprestimo WHERE nome_agencia='PUC');
 
 --05-Nomes de Clientes com depósitos, mas sem empréstimos na agência PUC;
 
-SELECT nome_cliente FROM deposito WHERE ='PUC'  
+SELECT nome_cliente FROM deposito WHERE nome_agencia='PUC'  
 except 
-SELECT nome_cliente  WHERE nome_agencia='PUC'
+SELECT nome_cliente FROM emprestimo WHERE nome_agencia='PUC'
 
-SELECT DISTINCT nome_cliente deposito WHERE nome_agencia='PUC' 
-AND nome_cliente NOT IN (SELECT DISTINCT  FROM emprestimo WHERE nome_agencia='');
+SELECT DISTINCT nome_cliente FROM deposito WHERE nome_agencia='PUC' 
+AND nome_cliente NOT IN (SELECT DISTINCT nome_cliente FROM emprestimo WHERE nome_agencia='PUC');
 
 --06-Clientes que possuem depósitos ou empréstimos na agencia da PUC
 
-SELECT DISTINCT nome_cliente 
+SELECT DISTINCT nome_cliente FROM deposito WHERE nome_agencia='PUC'
 UNION
 SELECT DISTINCT nome_cliente FROM emprestimo WHERE nome_agencia='PUC'
 
@@ -49,18 +49,18 @@ SELECT DISTINCT nome_cliente FROM emprestimo WHERE nome_agencia='PUC'
 
 SELECT nome_cliente
 FROM Cliente
-WHERE 'Belo Horizonte';
+WHERE cidade_cliente = 'Belo Horizonte';
 
 --08-Nomes de clientes que tem 'Santos' no nome
 
-SELECT nome_cliente
+SELECT nome_cliente FROM cliente
 WHERE nome_cliente LIKE '%Santos%';
 
 --09-Nomes de clientes que tem terminam com 'Souza' ou 'Sousa'
 
 SELECT nome_cliente
 FROM cliente
-WHERE nome_cliente = '%Sou_a';
+WHERE nome_cliente like '%Sou_a';
 
 --10-Total de depósitos para cada nome de cliente
 
@@ -72,26 +72,26 @@ FROM deposito GROUP BY nome_cliente;
 
 SELECT nome_cliente, saldo_deposito
 FROM deposito 
-WHERE saldo_deposito >= 4000 
+WHERE saldo_deposito >= 3000 
 AND  saldo_deposito <= 4000
 
 SELECT nome_cliente, saldo_deposito
 FROM deposito 
-WHERE saldo_deposito BETWEEN 3000 
+WHERE saldo_deposito BETWEEN 3000 and 4000
 
 --12-Nomes de clientes e a soma de depósitos de cada nome, 
 --quando a soma for maior que R$ 5.000,00
 
 SELECT nome_cliente, sum(saldo_deposito)
 FROM deposito GROUP BY nome_cliente
-HAVING sum() > 5000
+HAVING sum(saldo_deposito) > 5000
 
 --13-Nomes de clientes e a soma de depósitos de cada nome,
 --quando a soma estiver entre R$ 1.000,00 e R$ 4.000,00,
 --em ordem pela soma
 
 SELECT nome_cliente, sum(saldo_deposito)
-FROM deposito 
+FROM deposito GROUP BY nome_cliente
 HAVING sum(saldo_deposito) BETWEEN 1000 AND 4000
 ORDER BY sum(saldo_deposito)
 
@@ -102,7 +102,7 @@ ORDER BY sum(saldo_deposito)
 
 SELECT nome_cliente, sum(saldo_deposito)
 FROM deposito GROUP BY nome_cliente
-HAVING sum() > avg()
+HAVING sum(saldo_deposito) > avg(saldo_deposito)
 ORDER BY sum(saldo_deposito)
 
 --15-Faça uma listagem da soma de depósitos feitos
@@ -111,7 +111,7 @@ ORDER BY sum(saldo_deposito)
 
 SELECT nome_agencia, nome_cliente, sum(saldo_deposito)
 FROM deposito 
-GROUP BY nome_cliente
+GROUP BY nome_cliente, nome_agencia
 ORDER BY nome_cliente, nome_agencia
 
 --16- Selecione os nomes dos clientes e a soma de depósitos
@@ -124,12 +124,12 @@ FROM deposito GROUP BY nome_cliente, nome_agencia
 
 --Conjunto2: 
 SELECT sum(saldo_deposito) FROM deposito 
-WHERE  GROUP BY nome_agencia
+WHERE nome_agencia = 'Pampulha' GROUP BY nome_agencia
 
 --Conjunto1 HAVING sum > ALL Conjunto2
 SELECT nome_cliente, nome_agencia, sum(saldo_deposito) 
 FROM deposito GROUP BY nome_cliente, nome_agencia 
-HAVING sum() > ALL
+HAVING sum(saldo_deposito) > ALL
 (SELECT sum(saldo_deposito) FROM deposito 
-WHERE  GROUP BY nome_agencia);
+WHERE nome_agencia = 'Pampulha' GROUP BY nome_agencia);
 
